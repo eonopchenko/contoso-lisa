b07618040bc0e698c3bc19ef30abd54377515325
eonopchenko <jake.spb@gmail.com>
2017-11-26 23:45:48 +1300

    +Added sign up and sign in

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const helmet = require("helmet");
 const express_enforces_ssl = require("express-enforces-ssl");
 const app = express();
+const request = require('request');
 
 app.set('view engine', 'ejs');
 
@@ -26,19 +27,74 @@ app.use(session({
 
 ///--- INDEX PAGE ---///
 app.get('/',function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 ///--- REGISTERED LOGIN ---///
 app.get('/login',function(req, res) {
-  req.session.username = req.query.username;
-  req.session.password = req.query.password;
-  res.contentType('application/json');
-  res.send("{\"login\":\"success\"}");
-  // req.session.username = "";
-  // req.session.password = "";
-  // res.contentType('application/json');
-  // res.send("{\"login\":\"error\"}");
+
+  request({
+    headers: {
+      'ZUMO-API-VERSION': '2.0.0',
+      'Content-Type': 'application/json'
+    },
+    uri: 'http://contoso-lisa-mobile.azurewebsites.net/tables/CustomerTable',
+    method: 'GET'
+  }, function (error, response, body) {
+    if(error != null) {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      console.log('body:', body);
+    } else {
+      var customers = JSON.parse(body);
+      var success = false;
+      for(var index in customers) {
+        console.log(customers[index].username);
+        console.log(customers[index].password);
+        if((customers[index].username == req.query.username) && (customers[index].password == req.query.password)) {
+          req.session.username = req.query.username;
+          res.contentType('application/json');
+          res.send("{\"login\":\"success\"}");
+          success = true;
+          break;
+        }
+      }
+    }
+    
+    if(!success) {
+      req.session.username = "";
+      res.contentType('application/json');
+      res.send("{\"login\":\"error\"}");
+    }
+  });
+});
+
+///--- SUBMIT ---///
+app.get('/submit',function(req, res) {
+
+  var data = '{' + 
+  '"username": "' + req.query.username + '",' + 
+  '"firstname": "' + req.query.firstname + '",' + 
+  '"lastname": "' + req.query.lastname + '",' + 
+  '"email": "' + req.query.email + '",' + 
+  '"tel": "' + req.query.tel + '",' + 
+  '"password": "' + req.query.password + '"}';
+
+  request({
+    headers: {
+      'ZUMO-API-VERSION': '2.0.0',
+      'Content-Type': 'application/json'
+    },
+    uri: 'http://contoso-lisa-mobile.azurewebsites.net/tables/CustomerTable',
+    body: data,
+    method: 'POST'
+  }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.contentType('application/json');
+    res.send("{\"submit\":\"success\"}");
+  });
 });
 
 app.get('/botchat',function(req, res) {
---
+++ views/resources/js/signup.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    $("#button-submit").click(function(e) {
+        e.preventDefault();
+
+        var username = $("#username-text-input").val();
+        var firstname = $("#firstname-text-input").val();
+        var lastname = $("#lastname-text-input").val();
+        var email = $("#email-input").val();
+        var tel = $("#tel-input").val();
+        var password1 = $("#password1-input").val();
+        var password2 = $("#password2-input").val();
+        var birthdate = $("#birthdate-input").val();
+
+        if(
+            (username == "") || 
+            (firstname == "") || 
+            (lastname == "") || 
+            (email == "") || 
+            (tel == "") || 
+            (password1 == "") || 
+            (password2 == "") || 
+            (password1 != password2)) {
+            $("#submit-danger").fadeTo(2000, 500).slideUp(500, function() {
+                $("#submit-danger").slideUp(500);
+            });
+        } else {
+            $.ajax({
+                url: "/submit",
+                type: "GET",
+                dataType: "json",
+                data: {
+                    username: username,
+                    firstname: firstname,
+                    lastname: lastname,
+                    email: email,
+                    tel: tel,
+                    password: MD5(password1)
+                },
+                contentType: "application/json",
+                cache: true,
+                timeout: 5000,
+                complete: function() {
+                    console.log('process complete');
+                },
+                success: function(data) {
+                    console.log('process success');
+                    document.location.href="/";
+                },
+                error: function() {
+                    console.log('process error');
+                    document.getElementById("submit-danger").style.display="block";
+                },
+            });
+        }
+    });
+
+    // $("#id_a_goback").click(function(e) {
+    //     e.preventDefault();
+    //     window.location.replace("/");
+    // })
+});
